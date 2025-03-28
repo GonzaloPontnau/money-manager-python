2526e5e151ddba878de79801f5f185daba0106f7
Gonzalo Pontnau <gonzalo92003@gmail.com>
2025-03-28 00:57:07 -0300

    fix: actualizar la configuración de archivos estáticos para Vercel y simplificar el script de construcción

--- money_manager/settings.py
+++ money_manager/settings.py
@@ -172,13 +172,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'  # Cambiar '/staticfiles/' a '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configuración adicional para archivos estáticos
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Añadir configuración adicional para WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configuración para entorno de Vercel
 if 'VERCEL' in os.environ:
