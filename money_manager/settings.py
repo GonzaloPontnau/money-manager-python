19f8cabf520f02ee459a8c5ac2813374ab5edcc4
Gonzalo Pontnau <gonzalo92003@gmail.com>
2025-03-28 00:42:53 -0300

    fix: actualizar la URL de archivos estáticos en la configuración y ajustar la ruta en vercel.json

--- money_manager/settings.py
+++ money_manager/settings.py
@@ -172,7 +172,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configuración adicional para archivos estáticos
